6dd5b81b176458e163ef6a344ba3934ad2df9069
Syed Danish Pasha <sydanishp@gmail.com>
2023-02-27 22:25:27 +0530

    details

---
+++ script.js
@@ -0,0 +1,91 @@
+var players = [
+    {
+    "id": 0,
+    "playerName": "Hardik Pandya",
+    "imgPlayer" : "https://www.mumbaiindians.com/static-assets/images/players/small/63751.png",
+    "from": "MI",
+    "price": "6.50 Cr",
+    "isPlaying": true,
+    "playerDescription" : "If there is one thing that separates Hardik Pandya from other young cricketers, it is his immense self-belief. The flamboyant Baroda all-rounder never lets pressure affect his game, but instead relishes such situations. A big hitter and a fast-bowling all-rounder, Hardik ticks all the boxes that a team desires from an all-rounder. It was no surprise that he was fast-tracked into the Indian ODI and T20 side after a successful run in the IPL 2015. In his debut IPL game against RCB, he hit a six off just the second ball he faced. However, his potential was truly known when he took Mumbai Indians to victory with a quick-fire eight ball 21 against CSK coming when the team needed 30 from two overs. Since then he has been a vital cog in the Mumbai Indians line-up. This year though he will be making his debut as captain for the new franchise Gujarat Titans. Hardik will be leading a side that boasts of a good blend of youth and experience. His credentials as an all-rounder are well known but how will he lead this new franchise? Only time will tell.",
+    "description": "All-rounder"
+    },
+    {
+    "id": 1,
+    "playerName": "Virat Kohli",
+    "imgPlayer" : "https://bcciplayerimages.s3.ap-south-1.amazonaws.com/playerheadshot/ipl/284/164.png",
+    "from": "RCB",
+    "price": "8.00 Cr",
+    "isPlaying": true,
+    "playerDescription" : "Ahead of the start of 14th edition of the league, Virat Kohli announced that this would be his final season as Captain of the RCB. Virat Kohli who took over as RCB's full-time captain in 2013, led them in 140 matches, winning 66 and losing as many as 70. What the former RCB Skipper also confirmed to the fans ahead of the IPL Mega Auction was that he will always remain and RCB player, loyal to his franchise. He was picked in the IPL draft ahead of the IPL 2022 mega auction for INR 15 crores. The former Indian Cricket Team’s Captain is popularly known as the Run Machine. Virat Kohli’s ability and hunger to score daddy hundreds has taken the cricket world by storm. The flamboyant cricketer was the captain of the Indian team that won the 2008 ICC U19 Cricket World Cup and since then there has been no looking back for King Kohli. He is the new-age cricketer who is aggressive and expresses his mind on the field. In 2008, Kohli was inducted in the RCB squad and started leading the side in the year 2013. In the year 2016, he sat atop on the leading run-scorers list, thereby donning the Orange Cap at the end of the ninth edition of the league. Until 2022, he was the highest paid cricketer in IPL, and was the first one to be retained by his franchise ahead of the IPL 2018 Player Auction.",
+    "description": "Batsman"
+    },
+    {
+    "id": 2,
+    "playerName": "Yuvraj Singh",
+    "imgPlayer" : "https://www.mumbaiindians.com/static-assets/images/players/large/3314.png?v=2.19&w=400",
+    "from": "MI",
+    "price": "1.00 Cr",
+    "isPlaying": false,
+    "playerDescription" : "Yuvraj Singh is one of the finest batsmen in world cricket. Irrespective of the format he plays, the left-handed batsman takes on the opposition with authority. The left-hander played and important role in India's World Cup winning triumph in 2011 and was adjudged man of the tournament for the same. India lifted the trophy for the second time in history of the game. Yuvraj also achieved a rare feat when he scored six sixes in one over. He smashed Stuart Broad during the inaugural T20 World Cup in 2007. India eventually won that tournament after beating arch rivals Pakistan in the final encounter. In the past seasons, Yuvraj has been a part of Kings XI Punjab, Sunrisers Hyderabad, Delhi Daredevils, Royal Challengers Bangalore and Pune Warriors India. In 2018 edition, it would be a home coming for the aggressive batsman as Yuvraj will be joing his first IPL franchise Kings XI Punjab.",
+    "description": "Batsman"
+    },
+    {
+    "id": 3,
+    "playerName": "Chris Morris",
+    "imgPlayer" : "https://bcciplayerimages.s3.ap-south-1.amazonaws.com/playerheadshot/ipl/284/836.png",
+    "from": "RR",
+    "price": "16.25 Cr",
+    "isPlaying": true,
+    "playerDescription" : "A bowling all-rounder from South Africa, Chris Morris is a naturally gifted athlete, and can turn a game of cricket around with both bat and ball. Morris often bowls at speeds beyond 140kph and bats fearlessly in the lower order. With the ability to crank up the speed gun while generating good bounce, and some well-documented hitting prowess, he makes for a genuine all-round option.The 30-year-old Morris made his debut for the Delhi franchise in 2016 and finished as their leading wicket-taker in that season with 13 wickets. His destructive batting down the order has always come in handy as well. In his debut ODI series vs England in 2016, with South Africa 1-2 down and on the brink of losing the 4th ODI as well, Morris turned the match around from out of nowhere and stunned England with his power-hitting, scoring 62 off just 38 balls. A bowler capable of generating disconcerting pace and a batsman who can hit the ball hard, Morris has taken 55 wickets and scored 485 runs in the IPL.",
+    "description": "All-rounder"
+    },
+    {
+    "id": 4,
+    "playerName": "Glenn Maxwell",
+    "imgPlayer" : "https://bcciplayerimages.s3.ap-south-1.amazonaws.com/playerheadshot/ipl/284/282.png",
+    "from": "RCB",
+    "price": "14.25",
+    "isPlaying": true,
+    "playerDescription" : "All-rounder Glenn Maxwell was one of the three players retained by the RCB ahead of IPL 2022. Glenn Maxwell was retained for INR 11 crores. The 2021 edition of the league saw Maxwell amass 513 runs in 15 matches with a strike rate of 144.10. A destructive batsman, Glenn Maxwell has lit up the shortest format of the game with his batting exploits and power hitting. Maxwell is one cricketer who has always generated tremendous interest at the IPL auction. A brilliant fielder, who can contribute with his off-spin, the Australian can play the role of a floater in a line-up. His one of the best seasons was in 2014 when Maxwell teamed up with David Miller for Kings XI Punjab in clobbering the opposition and he now again returns to KXIP. The franchise went all out for him and could finally secure his services at a whopping INR 10.75 crore. Maxwell was released by Punjab Kings (then KXIP) ahead of the 2021 edition of the league and was then bought by RCB for a whooping INR 14.25 crores in the Player Auction.",
+    "description": "All-rounder"
+    },
+    {
+    "id": 5,
+    "playerName": "Rohit Sharma",
+    "imgPlayer" : "https://www.mumbaiindians.com/static-assets/images/players/large/3852.png?v=2.19&w=200",
+    "from": "MI",
+    "price": "6.50 Cr",
+    "isPlaying": true,
+    "playerDescription" : "Captain Marvellous. Rohit Sharma has etched his name as the most successful captain in the IPL with five trophies, the most by anyone in the tournament. He has cracked the code and made MI the most consistent team. A prolific run-getter, the Hitman is one of the most naturally gifted batsmen in the world and has been enjoying a tremendous run with the bat. When it comes to the shorter format, there are few who can match his striking abilities. Rohit proved why he is regarded as a special talent when he became the first batsman to strike two double centuries in the ODIs and he now has three. He was one of the most consistent Deccan Chargers batsmen in the first three seasons of the IPL and was instrumental in his former franchise winning the crown in 2009 However, since 2011, Rohit has been an integral part of the Mumbai Indians guiding their batting line-up. He took over the team’s captaincy a couple of games into the 2013 season and led MI to their maiden IPL title. In the same year, MI also completed a grand double when they clinched the Champions League T20 title. His captaincy was put to test in the 2015 season after the team won just one of their first six games and were struggling to stay afloat. Mumbai Indians then staged a remarkable turnaround and lifted the Trophy for the second time with a convincing win in the final. Rohit, who made a half-century, was named the Player of the Match. The legacy of Hitman expanded further on, after powering Mumbai Indians to title victories in the 2017 and 2019 seasons, both being last ball thrillers. Rohit led the Mumbai Indians to a successful campaign in the 2020 season, which was their second consecutive title. MI did not qualify for playoffs in the 14th season and will be raring to go this time. He now returns as Team India’s full-time captain in all three formats of the game.",
+    "description": "BatsMan"
+    },
+    {
+    "id": 6,
+    "playerName": "Ishan Kishan",
+    "imgPlayer" : "https://www.mumbaiindians.com/static-assets/images/players/large/64712.png?v=2.21&w=200",
+    "from": "MI",
+    "price": "2.50 Cr",
+    "isPlaying": true,
+    "playerDescription" : "It was a toss-up between the reliable Aditya Tare and the explosive Ishan Kishan ahead of the 11th season and Mumbai Indians opted for Kishan. From that day till now, nothing really has changed and Ishan’s stocks have only soared. The Mumbai Indians didn’t want to let him go and pulled out all stops as they spent a whopping 15.25 crore to have him back. It was the most expensive buy at the mega auction ahead of IPL 15. Now that Ishan returns home, the MI will count on him to carry on his aggressive approach and give the team the kind of starts required in the powerplay.",
+    "description": "BatsMan"
+    }
+];
+
+
+var playerDetails = document.getElementById("playerCardDiv");
+var heading = document.getElementById('heading');
+var head_title = document.createElement('h1');
+head_title.innerHTML = "Details"
+
+for (var i = 0; i < players.length; i++){
+    if(players[i].isPlaying === false){
+        var playerCard = document.createElement("div");
+        playerCard. = "card";
+
+        var divImg = document.createElement('div');
+        divImg.className = "img";
+        var playImg = document.createElement('img');
+        playImg.src = players[i].imgPlayer;
+        divImg.append(playImg);
+    }
+}
